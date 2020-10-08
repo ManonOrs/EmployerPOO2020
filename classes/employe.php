422fe07554818aa30b6cb3ba83891e5d67acd7eb
manon <manon.orsini4@gmail.com>
2020-10-08 10:25:32 +0200

    Classe employe constructeur get et set

---
+++ classes/employe.php
@@ -0,0 +1,43 @@
+<?php
+
+class employe {
+    protected int $numero;
+    protected string $nom;
+    protected string $prenom;
+    protected DateTime $dateDeNaissance;
+    protected float $salaireM;
+    
+    public function __construct(int $pnum, string $pnom, string $pprenom, DateTime $pddn, float $psalaire) {
+        $this->numero = $pnum;
+        $this->nom = $pnom;
+        $this->prenom = $pprenom;
+        $this->dateDeNaissance = $pddn;
+        $this->salaireM = $psalaire;
+    }
+    
+    function getNumero(): int {
+        return $this->numero;
+    }
+
+    function getNom(): string {
+        return $this->nom;
+    }
+
+    function getPrenom(): string {
+        return $this->prenom;
+    }
+
+    function getDateDeNaissance(): DateTime {
+        return $this->dateDeNaissance;
+    }
+
+    function getSalaireM(): float {
+        return $this->salaireM;
+    }
+
+    function setSalaireM(float $salaireM): void {
+        $this->salaireM = $salaireM;
+    }
+
+    
+}
